0e378207dad791c01773c6f506672acf25cf25b3
zgx <1637349696@qq.com>
2024-01-09 22:59:10 +0800

    feat: requirements

--- pyOS/settings.py
+++ pyOS/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -27,20 +26,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'simpleui',                     # 注册simpleui应用
+    'simpleui',  # 注册simpleui应用
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fileSys.apps.FilesysConfig',   # 注册文件系统应用
-    'apps.apps.AppsConfig',         # 注册应用管理应用
-    'user.apps.UserConfig',         # 注册用户管理应用
+    'fileSys.apps.FilesysConfig',  # 注册文件系统应用
+    'apps.apps.AppsConfig',  # 注册应用管理应用
+    'user.apps.UserConfig',  # 注册用户管理应用
 ]
 
 MIDDLEWARE = [
@@ -75,17 +73,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pyOS.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'minios',
+        'USER': 'minios',
+        'PASSWORD': 'minios',
+        'HOST': '218.244.146.160',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -105,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -116,7 +122,6 @@ TIME_ZONE = 'Asia/Shanghai'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
