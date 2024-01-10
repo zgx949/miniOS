b9850340de6547571fd655caa3edfb09d955e6be
zgx <1637349696@qq.com>
2024-01-10 20:17:20 +0800

    feat: 新增viewer

---
+++ vue/mini_os/public/viewer/js/about.e2c95a51.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(e,n,t){"use strict";t.r(n);var c=t("7a23");const o={class:"about"},a=Object(c["createElementVNode"])("h1",null,"This is an about page",-1),s=[a];function r(e,n){return Object(c["openBlock"])(),Object(c["createElementBlock"])("div",o,s)}var u=t("6b0d"),b=t.n(u);const i={},l=b()(i,[["render",r]]);n["default"]=l}}]);
+//# sourceMappingURL=about.e2c95a51.js.map
